4c81bbeff16aab9f22d4a7e4db24881b9774bfc1
Tim Beckett <timbeckett@Tims-MacBook-Pro.local>
2017-04-06 14:54:44 -0400

    Fixed bug in app.js portfolio sorting

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -52,9 +52,9 @@ $(document).ready(function($) {
       //   }
       // });
 
-      $('button').on('click', function() {
-        $(this).parents('#projects').find('li').removeClass('show');
-        $(this).parents('#projects').find('li').addClass('hide');
+    $('button').on('click', function() {
+      $(this).parents('#projects').find('li').removeClass('show');
+      $(this).parents('#projects').find('li').addClass('hide');
    });
 
    $('#personal').on('click', function() {
